2f5adac86199b2ef5a67b2fcea125fb430928871
Chris Dalke <chrisdalke@gmail.com>
2020-05-06 11:52:16 -0400

    Messing with navbar layout

--- components/features/navbar/NavBar.jsx
+++ components/features/navbar/NavBar.jsx
@@ -4,23 +4,27 @@ import {Button, Navbar, Alignment, Icon} from "@blueprintjs/core";
 
 function NavBar() {
     return (
-        <Navbar>
-            <Navbar.Group align={Alignment.LEFT}>
-                <Navbar.Heading>
-                    <a className="bp3-button bp3-minimal bp3-icon-airplane bp3-large">TJET</a>
-                </Navbar.Heading>
-            </Navbar.Group>
-            <Navbar.Group align={Alignment.RIGHT} className={styles.desktopMenu}>
-                <a className="bp3-button bp3-minimal">About</a>
-                <a className="bp3-button bp3-minimal">Updates</a>
-                <a className="bp3-button bp3-minimal">Source Code</a>
-                <a className="bp3-button bp3-minimal">Documentation</a>
-                <Navbar.Divider />
-                <a className="bp3-button bp3-minimal bp3-intent-primary">Sign In</a>
-            </Navbar.Group>
-            <Navbar.Group align={Alignment.RIGHT} className={styles.mobileMenu}>
-                <a className="bp3-button bp3-icon-menu bp3-minimal" />
-            </Navbar.Group>
+        <Navbar className={styles.navBar}>
+            <div className="bp3-dark">
+            <div className={styles.navBarContent}>
+                <Navbar.Group align={Alignment.LEFT}>
+                    <Navbar.Heading>
+                        <a className="bp3-button bp3-minimal bp3-icon-airplane bp3-large">TJET</a>
+                    </Navbar.Heading>
+                </Navbar.Group>
+                <Navbar.Group align={Alignment.RIGHT} className={styles.desktopMenu}>
+                    <a className="bp3-button bp3-minimal">About</a>
+                    <a className="bp3-button bp3-minimal" href="https://docs.telemetryjet.com/">Docs</a>
+                    <a className="bp3-button bp3-minimal" href="https://files.telemetryjet.com/">Releases</a>
+                    <a className="bp3-button bp3-minimal">Blog</a>
+                    <Navbar.Divider />
+                    <a className="bp3-button bp3-intent-primary">Sign In</a>
+                </Navbar.Group>
+                <Navbar.Group align={Alignment.RIGHT} className={styles.mobileMenu}>
+                    <a className="bp3-button bp3-icon-menu bp3-minimal" />
+                </Navbar.Group>
+            </div>
+            </div>
         </Navbar>
     );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,25 +2,6 @@ import React from "react";
 import BasePage from "../components/screens/BasePage";
 import PageBlock from "../components/features/page/PageBlock";
 
-/*
-<h1><span className="logo-text">TelemetryJet</span> is in early development.</h1>
-<b>TelemetryJet</b> is a real-time data and control platform for makers.
-<ul className="bp3-list">
-    <li>Stream live data sensor data from your project, with an easy-to-use set of libraries and
-        integrations.
-    </li>
-    <li>
-        View, record, and analyze live data from any internet-connected device.
-    </li>
-    <li>
-        Build beautiful, functional dashboards and share with teammates.
-    </li>
-    <li>
-        Trigger actions based on data, and feed control signals back to devices.
-    </li>
-</ul>
- */
-
 function Index() {
     return (
         <BasePage>
